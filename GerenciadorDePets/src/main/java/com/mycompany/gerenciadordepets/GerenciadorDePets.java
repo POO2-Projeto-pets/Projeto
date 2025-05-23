15e9c7a95c9ac958d47d2396fddf227e1fcb0d87
Leonardo S. Cruz <>
2025-05-22 21:50:58 -0300

    teste hahaha leonardo

--- GerenciadorDePets/src/main/java/com/mycompany/gerenciadordepets/GerenciadorDePets.java
+++ GerenciadorDePets/src/main/java/com/mycompany/gerenciadordepets/GerenciadorDePets.java
@@ -11,6 +11,6 @@ package com.mycompany.gerenciadordepets;
 public class GerenciadorDePets {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! hahahah");
     }
 }
