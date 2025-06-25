367ff4a92cc8dbf8b05dc1d6086d5a7a37a9d493
LucasTheobaldo <lucthe@alunos.utfpr.edu.br>
2025-06-25 14:19:35 -0300

    - Adição das chaves primarias de Pessoa a Servico.

--- GerenciadorDePets/src/main/java/controller/EnderecoBD.java
+++ GerenciadorDePets/src/main/java/controller/EnderecoBD.java
@@ -5,8 +5,10 @@
 package controller;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import model.Endereco;
 
 /**
  *
@@ -24,11 +26,10 @@ public class EnderecoBD {
         
     public void criaTable() {
         String sqlCreate = "CREATE TABLE IF NOT EXISTS Endereco ("
-            + "uid int not null primary key,"
+            + "uid SERIAL primary key,"
             + "estado text,"
             + "cidade text,"
-            + "nomeRua text,"
-            + "cep text UNIQUE NOT NULL,"
+            + "rua text,"   
             + "numero int"                
             + ")";
         try{
@@ -58,6 +59,32 @@ public class EnderecoBD {
             con.close();
         }catch(ClassNotFoundException | SQLException e){
             System.out.println("Erro ao criar relacão usuario_endereco..."); 
+            System.out.println(e);
+        }
+    }
+    
+    PreparedStatement ps = null;
+    
+    public void inserir(Endereco endereco){
+        String sql1 = " INSERT INTO Endereco VALUES(?,?,?,?,?)";
+        /*String sql1 = "UPDATE Endereco SET estado = (?), cidade = (?), rua = (?), numero  = (?)"
+                + "WHERE uid = '1'";  */
+        try{
+            Class.forName(driver);
+            con = DriverManager.getConnection(url,user,senha);
+            System.out.println("Inserindo dados de Endereço...");         
+            ps = con.prepareStatement(sql1);
+            System.out.println(ps);
+            ps.setInt(1, endereco.getUid());
+            ps.setString(2, endereco.getEstado());
+            ps.setString(3, endereco.getCidade());
+            ps.setString(4, endereco.getRua());
+            ps.setInt(5, endereco.getNumero());  
+            ps.execute();
+            System.out.println("Dados inseridos com sucesso!");
+            ps.close();
+            con.close();
+        }catch(ClassNotFoundException | SQLException e){
             System.out.println(e);
         }
     }
